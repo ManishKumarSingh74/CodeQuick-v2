a7ccc0b58e0d948f04318238efc663ff5c452085
ManishKumarSingh74 <ms74882395@gmail.com>
2026-03-18 10:54:18 +0530

    Change CORS origin to production URL

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const cors = require('cors')
 require('dotenv').config({ path: '../.env' });
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://codequick-1.onrender.com',
     credentials: true
 }))
 
