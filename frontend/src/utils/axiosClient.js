f07660ed501dce0f1e4bd93bf39334b3e6c44df9
Manish <ms74882395@gmail.com>
2026-03-25 09:46:16 +0530

    [frontend] some util conflict resolved

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,17 +1,10 @@
 import axios from "axios"
-
-<<<<<<< Updated upstream
-const axiosClient =  axios.create({
-    baseURL: 'https://codequick-v1.onrender.com',
-=======
 const axiosClient = axios.create({
-    baseURL: '/api',
->>>>>>> Stashed changes
+    baseURL: 'https://codequick-v1.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
     }
 });
-
 
 export default axiosClient;
